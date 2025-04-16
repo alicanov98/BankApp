78b6f01e194cf44d59f27d6af1d8562f5aca752e
alicanov98 <alicanov98@gmail.com>
2025-04-16 16:42:01 +0400

    Initial Commit

---
+++ BankApp/BankAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankAppApp.swift
+//  BankApp
+//
+//  Created by Malik Alijanov on 16.04.25.
+//
+
+import SwiftUI
+
+@main
+struct BankAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
