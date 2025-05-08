ea31294dfececc4e09be193de646d3169c2b36e5
alicanov98 <alicanov98@gmail.com>
2025-05-09 01:32:15 +0400

    Create Skeleton Loading and other pages

---
+++ BankApp/View/Components/SkletonView.swift
@@ -0,0 +1,18 @@
+//
+//  SkletonView.swift
+//  BankApp
+//
+//  Created by Malik Alijanov on 08.05.25.
+//
+
+import SwiftUI
+
+struct SkletonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkletonView()
+}
---
+++ BankApp/View/MainView/AddNewCard.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewCard.swift
+//  BankApp
+//
+//  Created by Malik Alijanov on 08.05.25.
+//
+
+import SwiftUI
+
+struct AddNewCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewCard()
+}
